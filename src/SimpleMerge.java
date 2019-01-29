5094e8f0776d0b1dd5e173a32a0e51cf5aff1e80
ntan1055 <43207464+ntan1055@users.noreply.github.com>
2019-01-29 09:32:08 -0500

    Update SimpleMerge.java

--- src/SimpleMerge.java
+++ src/SimpleMerge.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.Array;
+
 /**
  * Created by Teacher on 1/28/2019.
  * simpleMerge takes in two sorted arrays of
@@ -5,8 +7,15 @@
  * array of integers.
  */
 public class SimpleMerge {
-    public static int[] simpleMerge(int[] arr1, int[] arr2) {
+    public static int[] simpleMerge(int[] arr1, int[] arr2)
+    {
+        int length1 = arr1.length;
+        int length2 = arr2.length;
+        
+        int[] merge = (int[]) Array.newInstance(arr1.getClass().getComponentType(), length1 + length2);
+        System.arraycopy(arr1, 0, merge, 0, length1);
+        System.arraycopy(arr2, 0, merge, length1, length2);
 
+        return merge;
     }
-
 }
