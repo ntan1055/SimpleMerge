48ff449ff20e7c1cf31d2204d13f44272a1557d2
ntan1055 <43207464+ntan1055@users.noreply.github.com>
2019-01-29 09:36:07 -0500

    Update SimpleMerge.java

--- src/SimpleMerge.java
+++ src/SimpleMerge.java
@@ -11,10 +11,23 @@ public class SimpleMerge {
     {
         int length1 = arr1.length;
         int length2 = arr2.length;
-        
+
         int[] merge = (int[]) Array.newInstance(arr1.getClass().getComponentType(), length1 + length2);
         System.arraycopy(arr1, 0, merge, 0, length1);
         System.arraycopy(arr2, 0, merge, length1, length2);
+        
+        for(int i = 1; i < merge.length; i++)
+        {
+            for(int j = i; j > 0; j--)
+            {
+                if(merge[j] < merge[j-1])
+                {
+                    int sub = merge[j];
+                    merge[j] = merge[j-1];
+                    merge[j-1] = sub;
+                }
+            }
+        }
 
         return merge;
     }
